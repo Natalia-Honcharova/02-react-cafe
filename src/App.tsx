2db3dbae66efdbdb4c9dcbfd6637e55bc9cac9d5
Natalia <164662428+Natalia-Honcharova@users.noreply.github.com>
2026-07-23 14:09:16 +0300

    Add notification and conditional reset button

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import css from "./App.module.css";
+import CafeInfo from "./components/CafeInfo/CafeInfo";
+import { useState } from "react";
+import type { Votes, VoteType } from "./types/votes";
+import VoteOptions from "./components/VoteOptions/VoteOptions";
+import VoteStats from "./components/VoteStats/VoteStats";
+import Notification from "./components/Notification/Notification";
+
+export default function App() {
+
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  const handleVote = (type: VoteType) => {
+  setVotes({
+    ...votes,
+    [type]: votes[type] + 1,
+  });
+  };
+  
+  const handleReset = () => {
+  setVotes({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+};
+  const totalVotes =
+    votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes
+  ? Math.round((votes.good / totalVotes) * 100)
+  : 0;
+  
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+
+  
+      <VoteOptions
+       onVote={handleVote}
+       onReset={handleReset}
+       canReset={totalVotes > 0}
+      />
+    {totalVotes > 0 ? (
+     <VoteStats
+       votes={votes}
+       totalVotes={totalVotes}
+       positiveRate={positiveRate}
+        />
+        ) : (
+  <Notification />
+)}
+    </div>
+  );
+}
+
+
